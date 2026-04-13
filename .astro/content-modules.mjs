1ee49d2c05294e91acdc8c075c837683e7d53ebd
Proibito04 <edoardobalzano01@gmail.com>
2026-04-13 23:25:39 +0300

    feat: implement ASTCodeBlock component with syntax highlighting and output toggling support

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,1 @@
-
-export default new Map([
-["src/content/writeups/Silentium.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriteups%2FSilentium.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
       defaultColor: false,
       themes: {
         light: 'github-light',
-        dark: 'github-dark',
+        dark: 'github-dark-default',
       },
     }
   },
